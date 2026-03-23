988cd58f14a0855623cb2b9db68d20c44a92d34c
Sarvarbek <sardorbekozodov82@gmail.com>
2026-03-23 14:09:37 +0500

    Salom dunyo

--- src/dasturlash/Selsiy.java
+++ src/dasturlash/Selsiy.java
@@ -1,19 +1,5 @@
-//package dasturlash;
-//
-//import encapsulation.Encapsulation;
-//
-//public class Selsiy {
-//    public static void main(String[] args) {
-//Encapsulation c = new Encapsulation();
-//c.getName(Muso);
-//c.getSurname(Ozodov);
-//c.getgmail(muso@gmail.com);
-//
-//System.out.println(c.getName());
-//System.out.println(c.getSurname());
-//System.out.println(c.getgmail());
-//
-//System.out.println();
-//
-//    }
-//}
+public class Selsiy {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
