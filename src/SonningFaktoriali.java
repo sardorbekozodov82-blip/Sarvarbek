1923268adcadc8d9615abc2a9267101bc4447021
Sarvarbek <sardorbekozodov82@gmail.com>
2026-03-23 14:18:54 +0500

    Sonning foqtoryoli

---
+++ src/SonningFaktoriali.java
@@ -0,0 +1,12 @@
+public class SonningFaktoriali {
+    public static void main(String[] args) {
+        int n = 5;
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Faktorial: " + fact);
+    }
+}
