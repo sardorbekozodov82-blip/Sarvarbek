a656b9c8e3427b1a43c8eedd29f3c062d8fd6ba1
Sarvarbek <sardorbekozodov82@gmail.com>
2026-03-23 14:26:12 +0500

    Foydalanuvchidan son olish

---
+++ src/FoydalanuvchidanSonOlish.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class FoydalanuvchidanSonOlish {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Son kiriting: ");
+        int num = sc.nextInt();
+
+        System.out.println("Kiritilgan son: " + num);
+    }
+}
