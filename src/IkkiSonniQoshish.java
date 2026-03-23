c7a75536e06d012ad2320e2d43153aafb5d450b1
Sarvarbek <sardorbekozodov82@gmail.com>
2026-03-23 14:12:03 +0500

    sonlarni qo'shosh

---
+++ src/IkkiSonniQoshish.java
@@ -0,0 +1,8 @@
+public class IkkiSonniQoshish {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("Yig‘indi: " + sum);
+    }
+}
