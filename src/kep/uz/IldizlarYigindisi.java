19ac24710ca130278df4eef10dfce334d7c17441
Sarvarbek <sardorbekozodov82@gmail.com>
2026-03-28 13:43:38 +0500

    ildizlar yig'indisi

---
+++ src/kep/uz/IldizlarYigindisi.java
@@ -0,0 +1,28 @@
+package kep.uz;
+
+import java.util.Scanner;
+
+public class IldizlarYigindisi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            long yigindi = 0;
+
+
+            for (int i = 1; i <= n; i++) {
+
+                int butunQism = (int) Math.sqrt(i);
+
+
+                yigindi += butunQism;
+            }
+
+            System.out.println(yigindi);
+        }
+
+        sc.close();
+    }
+}
